18747ee71abac3caf0d1b34907c6dfcdc87fff07
elianalopezv <eliana.lopez@yuxiglobal.com>
2018-01-27 00:13:17 -0500

    scene change

---
+++ Jam2018/Assets/Scripts/OrderManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderManager : MonoBehaviour {
+
+	public static OrderManager Instance;
+	public List<int> orderStack = new List<int>();
+
+	void Awake()
+	{
+		if(Instance == null)
+			Instance = this;
+
+
+	}
+
+	public void AddOrder(string command)
+	{
+		switch (command)
+		{
+			case "m_LeftArrow":
+				orderStack.Add (1);
+				break;
+
+		default:
+			Debug.Log ("Not a command!");
+			break;
+
+		}
+		
+	}
+
+	public void CleanOrderStack ()
+	{
+		orderStack.Clear ();
+	}
+		
+}
--- Jam2018/Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
+++ Jam2018/Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
@@ -84,6 +84,10 @@ public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandle
         // Enable canvas':
         foreach (var component in canvasComponents)
             component.enabled = true;
+		
+		//Call Orders:
+		OrderManager.Instance.AddOrder(mTrackableBehaviour.TrackableName);
+
     }
 
 
