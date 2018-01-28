57578732f5d0c9441f37fa7de4e3b9731fdc4f28
Andres <andresgt.6@gmail.com>
2018-01-28 13:28:09 -0500

    time

--- Jam2018/Assets/Scripts/TimeManager.cs
+++ Jam2018/Assets/Scripts/TimeManager.cs
@@ -5,25 +5,25 @@ using UnityEngine.UI;
 
 public class TimeManager : MonoBehaviour
 {
-    public float timer;
+    public float timer = 120f;
     public Text textTime;
 
     private void Update()
     {
-        timer += Time.deltaTime;
+        timer -= Time.deltaTime;
         int minutes = Mathf.FloorToInt(timer / 60F);
         int seconds = Mathf.FloorToInt(timer - minutes * 60);
         if(minutes < 10 && seconds < 10)
         {
-            textTime.text = "0" + minutes + ":" + "0" + seconds;
+            textTime.text = "0" +Mathf.Abs(minutes) + ":" + "0" + Mathf.Abs(seconds);
         }
         if(minutes < 10 && seconds >= 10)
         {
-            textTime.text = "0" + minutes + ":" + seconds;
+            textTime.text = "0" + Mathf.Abs(minutes) + ":" + Mathf.Abs(seconds);
         }
         if (minutes >= 10 && seconds > 10)
         {
-            textTime.text =  minutes + ":" + seconds;
+            textTime.text = Mathf.Abs(minutes) + ":" + Mathf.Abs(seconds);
         }
 
     }
