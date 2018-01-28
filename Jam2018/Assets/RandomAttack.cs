4b0c794a63d734a04e098790b8b50dd68d38b3ca
Johnatan Botero <johnatan@jbotero.com>
2018-01-28 14:31:01 -0500

    Arte cute

---
+++ Jam2018/Assets/RandomAttack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAttack : MonoBehaviour {
+
+
+	public Animator animator;
+	private int random;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		random = Random.Range (0, 1000);
+		if (random < 10)
+			animator.Play ("Attack");
+	}
+}
